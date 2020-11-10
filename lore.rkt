6a590b30a68f21c663ad037902939f684b5b8111
srfoster <lindseyhandley@gmail.com>
2020-11-10 14:13:15 -0800

    lore update

--- lore.rkt
+++ lore.rkt
@@ -10,14 +10,13 @@
 	 preview-image)
 
 (define (name)
-  "Voxel World")
+  "Voxel")
 
 (define (description)
   @md{
-    This work is voxels
+    This work is Voxels.
 
     @(div
-       (alert-warning (b "Note.") " We are currently adding new runes and will update this world when we do.")
        (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build.")
        )
     })
