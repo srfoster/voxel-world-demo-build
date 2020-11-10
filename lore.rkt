1868b12e00cbd41fc97eaddadd1d1ade99257b19
Stephen Foster <stephen@thoughtstem.com>
2020-11-10 13:07:02 -0800

    updating to conventions

--- lore.rkt
+++ lore.rkt
@@ -1,7 +1,8 @@
 #lang at-exp racket
 
 (require website-js
-	 racket/runtime-path)
+	 racket/runtime-path
+	 codespells/lore)
 
 (provide name
          description
@@ -15,14 +16,15 @@
   @md{
     This work is voxels
 
+    @(div
+       (alert-warning (b "Note.") " We are currently adding new runes and will update this world when we do.")
+       (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build.")
+       )
     })
 
 (define (rune-collections)
   (list
-    ;path convention is gross
-    @md{
-    @(a href: "/collections/illusions-of-flame/index.html" "Illusions of Flame")}
-    ))
+    'fire-particles))
 
 (define-runtime-path preview-image "images/preview.png")
 
