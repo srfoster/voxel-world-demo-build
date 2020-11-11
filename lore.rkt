7e7a0456051911d4e634c5b8bba304642c401e4b
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 10:12:17 -0800

    fix

--- lore.rkt
+++ lore.rkt
@@ -10,7 +10,6 @@
     This work is Voxels.
 
     @(div
-<<<<<<< HEAD
        (alert-warning (b "Note.") " We are currently adding new runes and will update this world when we do.")
        (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build."))
     }
