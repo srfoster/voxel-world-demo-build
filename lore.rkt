2087af67b117b1158d7d1a5b8dc9fb4bf3be15fb
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 10:06:45 -0800

    new lore

--- lore.rkt
+++ lore.rkt
@@ -1,30 +1,20 @@
 #lang at-exp racket
 
-(require website-js
-	 racket/runtime-path
-	 codespells/lore)
+(require codespells/lore)
 
-(provide name
-         description
-         rune-collections
-	 preview-image)
+(define-runtime-path preview-image "images/preview.png")
 
-(define (name)
-  "Voxel World")
-
-(define (description)
+(define-authored-work-lore
+  #:name "Voxel" 
+  #:description 
   @md{
     This work is voxels
 
     @(div
        (alert-warning (b "Note.") " We are currently adding new runes and will update this world when we do.")
-       (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build.")
-       )
-    })
-
-(define (rune-collections)
-  (list
-    'fire-particles))
-
-(define-runtime-path preview-image "images/preview.png")
+       (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build."))
+    }
+  #:rune-collections
+  (list 'fire-particles)
+  #:preview-image preview-image)
 
