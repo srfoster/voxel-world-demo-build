48ea58fa1f3df73c3c6ba9d0ad2b6f45606d30cf
Stephen R. Foster <stephen@thoughtstem.com>
2020-11-17 20:32:55 +0000

    added all collections

--- lore.rkt
+++ lore.rkt
@@ -12,6 +12,9 @@
        (alert-primary "In its current form, we estimate this build to be worth about " (b "5 minutes of fun") ". Walk through the infinite world.  Fall into ravines.  Try to get out again.  Grumble that we have not yet added the teleport Rune to this build."))
     }
   #:rune-collections
-  (list 'fire-particles)
+  (list 'hierarchy
+	'fire-particles
+	'ice-particles
+	'rocks)
   #:preview-image preview.png)
 
--- main.rkt
+++ main.rkt
@@ -2,17 +2,23 @@
 
 (require voxel-world-demo-build/mod-info)
 
+(require-mod hierarchy)
 (require-mod fire-particles)
+(require-mod ice-particles)
+(require-mod rocks)
 
 (define my-mod-lang
   (append-rune-langs #:name main.rkt  
-                     (fire-particles:my-mod-lang #:with-paren-runes? #t)))
-
+                     (hierarchy:my-mod-lang #:with-paren-runes? #t)
+                     (fire-particles:my-mod-lang)
+                     (ice-particles:my-mod-lang)
+                     (rocks:my-mod-lang)
+		     ))
 
 (module+ main
 
   (codespells-workspace ;TODO: Change this to your local workspace if different
-   (build-path (current-directory) ".." ".." "CodeSpellsWorkspace"))
+   (build-path (current-directory) ".." ".."))
 
   (once-upon-a-time
    #:world (voxel-world)
